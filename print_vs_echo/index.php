970e76fd593595c08dc85281d1fe5e02bb0029e3
Atik <rahmanatawur@gmail.com>
2019-04-17 02:49:01 +0600

    updated

--- print_vs_echo/index.php
+++ print_vs_echo/index.php
@@ -1,3 +1,7 @@
 <?php
- var_dump(echo( "I am the boss"));
+//echo "I am the boss","I am the second bosss","I am the third boss";
+//print "I am the boss","I am the second bosss","I am the third boss";
+//print("I am the Boss");
+//var_dump(echo( "I am the boss"));
+ var_dump(print( "I am the boss"));
 ?>
