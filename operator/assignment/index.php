4c3281b7976e83e96cd73e5b70f624cfc7aefe5d
Atik <rahmanatawur@gmail.com>
2019-05-06 00:19:30 +0600

    arithmatic operators

---
+++ operator/assignment/index.php
@@ -0,0 +1,20 @@
+<?php
+$first_number;
+$second_number;
+//echo "First Number =$first_number <br>";
+$first_number = 10;
+$second_number=100;
+echo "First Number =$first_number <br>";
+
+//$first_number += $second_number;//$first_number = $first_number+$second_number;
+
+//$first_number -= $second_number;//$first_number = $first_number-$second_number;
+
+//$first_number *= $second_number;//$first_number = $first_number*$second_number;
+//$first_number /= $second_number;//$first_number = $first_number/$second_number;
+
+$first_number -= $second_number;//$first_number = $first_number-$second_number;
+$first_number = $first_number-$second_number;
+echo "First Number =$first_number ";
+
+?>
