e3b5a32360682669d0955395b475e3943a47b393
Atik <rahmanatawur@gmail.com>
2019-06-12 00:20:14 +0600

    php classes part nine

---
+++ conditional_statement/index.php
@@ -0,0 +1,27 @@
+<?php
+ $number = 10;
+if($number > 10){
+    echo "value is getter than 10";
+
+} else if($number==10){
+    echo "value is equal to 10";
+}
+ else{
+    echo "value is less than 10";
+}
+ 
+$name ='mr nnn';
+
+if($name =='mr x'){
+    echo "Your are mr x";
+} else if($name=='mr y'){
+    echo "Your are mr y";
+}else if($name=='mr z'){
+   echo "Your are mr z";
+} else if($name=='mr n'){
+    echo "You are mr n";
+} else{
+    echo "You are none of them";
+}
+
+?>
